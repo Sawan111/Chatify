b3ec30b6446716ede74591fd0c54d9ec929714d8
Sawan111 <sks20jnv@gmail.com>
2026-02-25 06:35:24 +0530

    user db added for sign up

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+import {signup} from "../controllers/auth.controller.js";
+
 const router=express.Router();
 
-router.get("/signup",(req,res)=>{
-    res.send("Sign up Endpoint");
-});
+router.post("/signup",signup);
+
 router.get("/signin",(req,res)=>{
     res.send("Sign in Endpoint");
 });
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,24 +4,21 @@ import path from "path";
 import {connectDB} from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 const PORT = process.env.PORT;
 const app=express();
 import cors from "cors";
 
-app.use(cors({
-  origin: "https://chatiapp-tau.vercel.app",
-  credentials: true
-}));
+
 
 
 const __dirname=path.resolve();
 console.log(process.env.PORT);
-const API = import.meta.env.VITE_API_URL;
 
-axios.post(`${API}/api/auth/signup`, data);
-console.log(import.meta.env.VITE_API_URL);
 
+// req body
+app.use(express.json()); 
 
 app.use("/api/auth",authRoutes); 
 //make ready for deployment
