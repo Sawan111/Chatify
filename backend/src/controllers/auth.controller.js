7b0cbaa59ee134d2f66caaafd111639e6153bc1d
Sawan111 <sks20jnv@gmail.com>
2026-02-28 10:34:14 +0530

    login logout added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -59,3 +59,39 @@ export const signup = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+   
+  try{
+    const user=await User.findOne({email});
+    if(!user){
+      return res.status(400).json({message:"Invalid email or password"});
+    }
+
+    const isPasswordMatch=await bcrypt.compare(password,user.password);
+    if(!isPasswordMatch){
+      return res.status(400).json({message:"Invalid email or password"});
+    }
+
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+
+  } catch (error) {
+    console.log("Error in login controller:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = (_, res) => {
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    secure: ENV.NODE_ENV === "production",
+  });
+  res.status(200).json({ message: "Logged out successfully" });
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import {signup} from "../controllers/auth.controller.js";
+import {signup,login ,logout} from "../controllers/auth.controller.js";
 
 const router=express.Router();
 
 router.post("/signup",signup);
 
-router.get("/signin",(req,res)=>{
-    res.send("Sign in Endpoint");
-});
+router.post("/login",login);
+
+
+router.post("/logout",logout);
 
 
 export default router;
